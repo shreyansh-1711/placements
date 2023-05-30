b8b76f502d289453484db7603eeedd29eb85e2b1
Shreyansh Jain <shreyansh.jain251202@gmail.com>
2023-05-31 02:01:58 +0530

    Binary search

---
+++ franz leetcode sheet/binary search/Pow(x, n).cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    // double solve(double x, int n){
+    //     if(n==0){
+    //         return 1;
+    //     }
+    //     if(n==1){
+    //         return x;
+    //     }
+    //     for(int i)
+    //     return solve(x, n*x)
+    // }
+    double myPow(double x, int n) {
+        double a = pow(x,n);
+        return a;
+    }
+};
+
+int main() {
+
+    
+
+    return 0;
+}
