62e28d1eba97d652f99cbf6d9b5bfb0ff4821a5f
Shreyansh Jain <shreyansh.jain251202@gmail.com>
2023-06-02 01:53:54 +0530

    stack ques started

---
+++ dsa/stack and queues/Reverse String.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+       stack<int> st;
+        for(int i = 0 ; i < s.size() ; i++){
+            st.push(s[i]);
+        } 
+        int i = 0;
+        while(!st.empty()){
+            s[i]=st.top();
+            st.pop();
+            i++;
+        }
+    // reverse(s.begin(), s.end());
+    }
+};
+
+int main() {
+
+    
+
+    return 0;
+}
