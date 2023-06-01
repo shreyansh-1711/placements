a055d35a9c65ce0d99f270db077b8b1ea8ea6edd
Shreyansh Jain <shreyansh.jain251202@gmail.com>
2023-06-02 00:35:46 +0530

    flatten LL

---
+++ dsa/linked list/Flatten A Linked List.cpp
@@ -0,0 +1,63 @@
+// #include <bits/stdc++.h>
+
+// /****************************************************************
+
+//     Following is the class structure of the Node class:
+
+// class Node {
+// public:
+// 	int data;
+// 	Node* next;
+// 	Node* child;
+
+// 	Node(int data) {
+// 		this->data = data;
+// 		this->next = NULL;
+// 		this->child = NULL;
+// 	}
+// };
+
+// *****************************************************************/
+// Node* merge(Node* a,Node* b)
+// {
+// 	Node* temp=new Node(0);
+
+//     Node* res=temp;
+// 	while(a!=NULL and b!=NULL)
+// 	{
+// 		if(a->data<b->data)
+// 		{
+// 			temp->child=a;
+// 			temp=temp->child;
+// 			a=a->child;
+// 		}
+// 		else
+// 		{
+// 			temp->child=b;
+// 			temp=temp->child;
+// 			b=b->child;
+// 		}
+// 	}
+// 	if(a)
+// 	{
+// 		temp->child=a;
+// 	}
+// 	else
+// 	{
+// 		temp->child=b;
+// 	}
+// 	return res->child;
+// }
+
+// Node* flattenLinkedList(Node* head) 
+// {
+// 	// Write your code here
+// 	if(head==NULL || head->next==NULL)
+// 	return head;
+
+// 	head->next=flattenLinkedList(head->next);
+
+// 	head=merge(head,head->next);
+// 	head->next=NULL;
+// 	return head;
+// }
